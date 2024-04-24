3fa9ec146ee79dae11097392bd99c17f831de0c3
lucasmingatos <lucasmingatos@hotmail.com>
2024-04-24 11:23:27 -0300

    Commit final

---
+++ src/app/data/photoList.ts
@@ -0,0 +1,14 @@
+import { Photo } from "../type/Photo";
+
+export const photoList: Photo[] = [
+    { id: 1, url: '1.png'},
+    { id: 2, url: '1.png'},
+    { id: 3, url: '1.png'},
+    { id: 4, url: '1.png'},
+    { id: 5, url: '1.png'},
+    { id: 6, url: '1.png'},
+    { id: 7, url: '1.png'},
+    { id: 8, url: '1.png'},
+    { id: 9, url: '1.png'},
+    
+];
